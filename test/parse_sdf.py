00245a53a4fbe8605f83fed8af280fbaa0ca6c58
William Shen <william.w.y.shen@gmail.com>
2022-03-21 13:04:36 -0400

    Auto determine  directory

--- test/parse_sdf.py
+++ test/parse_sdf.py
@@ -1,13 +1,22 @@
+import os
 import pdb
 
 from urdf_parser_py.sdf import SDF
 
-sdf_tests = ['', 'collision_', 'geometry_', 'joint_', 'link_', 'visual_', 'm0m_0_']
+sdf_tests = ["", "collision_", "geometry_", "joint_", "link_", "visual_", "m0m_0_"]
 sdf_results = []
-for test_name in sdf_tests:
-    sdf_path = f"sdf_examples/{test_name}test.sdf"
-    print('Testing', sdf_path)
-    with open(sdf_path) as f:
-        sdf = SDF.from_xml_string(f.read())
-    sdf_results.append(sdf)
 
+
+def load_sdf_examples():
+    sdf_examples_dir = os.path.join(os.path.dirname(__file__), "sdf_examples")
+
+    for test_name in sdf_tests:
+        sdf_path = os.path.join(sdf_examples_dir, f"{test_name}test.sdf")
+        print("Testing", sdf_path)
+        with open(sdf_path) as f:
+            sdf = SDF.from_xml_string(f.read())
+        sdf_results.append(sdf)
+
+
+if __name__ == "__main__":
+    load_sdf_examples()
